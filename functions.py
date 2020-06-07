5d8980bcf9ff086fbc656dcf58fc0ee4bc313256
Henley74 <henley.bidon@gmail.com>
2020-06-07 06:18:29 +0200

    Delete functions.py

--- functions.py
+++
@@ -1,8 +0,0 @@
-def DetectHTcomplot(user):
-    liste_ht = '#GrandRemplacement OR #Soros OR #BushDid911 OR #ChinaDidCovid OR #ChinaDidCovid19 OR #pizzagates'
-
-    #RESTE A FAIRE : CONCATENER CETTE LISTE DE HT (la compléter peut-être) A UNE REQUETE DE RECHERCHE, EN Y INCLUANT LES ABONNEMENTS
-    #CCE QU'ON VA FAIRE C UN SYSTEME DE POINT GENRE UN POINT GAGNE PAR TWEET SUR HT LOUCHE ET UN DIXIEME DE PT OU MOINS GAGNE PAR COMPTE QUE LE MEC SUIT
-    #et qui A tweeté sur des ht fumeux, le tout peut etre ramné au nb tweet pcq sinon biaisé pour les gens qui ont juste énormément de tweet ou d'abo
-
-    var = 'e'
